14cbb34ec2a50bae16580c9d2d5e7cd4c46b0b0a
Juliano Monteiro <160036736+Juliano-Monteiro@users.noreply.github.com>
2024-05-24 12:31:26 -0300

    Atualização do Sistema Bancario em C
    > Criação da função de transferencia bancaria

--- 01 - Sistema Bancario em C/Banco_JMS.c
+++ 01 - Sistema Bancario em C/Banco_JMS.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <windows.h>
 #include <time.h>
+  // Necessario para usar Sleep()
 
-// Definição da estrutura de uma conta
+// DefiniÃ§Ã£o da estrutura de uma conta
 typedef struct {
     char Onome[50], email[50];
     char emprego[25];
@@ -12,21 +14,21 @@ typedef struct {
     long int codigo_conta;
 } conta;
 
-// Declaração das funções
-void interface();
+// DeclaraÃ§Ã£o das funï¿½ï¿½es
+void Pagina_interface();
 void criarConta(const char *url);
 void loginConta(const char *url);
 void IndexConta(const char *url, const char *nome, float saldo, long int codigo_conta);
 void sacar(const char *url, long int codigo);
 void depositarConta(const char *url, long int codigo);
-
+void transferir(const char *url,long int codigo);
 int main(void) {
-    interface();  // Chama a interface principal
+    Pagina_interface();  // Chama a Pagina_interface principal
     return 0;
 }
 
-// Função que exibe a interface principal do banco
-void interface() {
+// Funï¿½ï¿½o que exibe a Pagina_interface principal do banco
+void Pagina_interface() {
     system("cls || clear");  // Limpa a tela do console
     int opr;
     char url[] = "Dados.txt";  // Arquivo que armazena os dados das contas
@@ -41,7 +43,7 @@ void interface() {
     scanf("%d", &opr);
     getchar(); // Para consumir o '\n' deixado pelo scanf
 
-    // Chama a função apropriada com base na entrada do usuário
+    // Chama a funï¿½ï¿½o apropriada com base na entrada do usuï¿½rio
     switch (opr) {
         case 1:
             loginConta(url);
@@ -58,15 +60,15 @@ void interface() {
     }
 }
 
-// Função para criar uma nova conta
+// Funï¿½ï¿½o para criar uma nova conta
 void criarConta(const char *url) {
     system("cls || clear");  // Limpa a tela do console
     conta Conta;
     FILE *novaConta;
-    novaConta = fopen(url, "a");  // Abre o arquivo para adição de dados
-    srand((unsigned)time(NULL));  // Inicializa a semente do gerador de números aleatórios
+    novaConta = fopen(url, "a");  // Abre o arquivo para adiï¿½ï¿½o de dados
+    srand((unsigned)time(NULL));  // Inicializa a semente do gerador de nï¿½meros aleatï¿½rios
 
-    // Coleta de dados do usuário
+    // Coleta de dados do usuï¿½rio
     printf("Digite seu nome: ");
     fgets(Conta.Onome, sizeof(Conta.Onome), stdin);
     Conta.Onome[strcspn(Conta.Onome, "\n")] = '\0'; // Remove o caracter de nova linha \n
@@ -99,7 +101,7 @@ void criarConta(const char *url) {
     scanf("%ld", &Conta.salario);
     getchar(); // Para consumir o '\n' deixado pelo scanf
 
-    // Gera um código de conta aleatório
+    // Gera um cï¿½digo de conta aleatï¿½rio
     Conta.codigo_conta = rand();
     Conta.saldo = 0.0;  // Inicializa o saldo da conta como 0.0
 
@@ -113,11 +115,9 @@ void criarConta(const char *url) {
                 Conta.telefone, Conta.senha, Conta.salario, Conta.codigo_conta, Conta.saldo);
         fclose(novaConta);  // Fecha o arquivo
     }
-
-    interface();  // Retorna à interface principal
 }
 
-// Função para fazer login na conta
+// Funï¿½ï¿½o para fazer login na conta
 void loginConta(const char *url) {
     system("cls || clear");  // Limpa a tela do console
     FILE *logar;
@@ -127,7 +127,7 @@ void loginConta(const char *url) {
     int autenticado = 0;
     conta Conta;
 
-    // Solicita os dados de login do usuário
+    // Solicita os dados de login do usuï¿½rio
     printf("E-mail: ");
     fgets(email, sizeof(email), stdin);
     email[strcspn(email, "\n")] = '\0'; // Remove o caracter de nova linha \n
@@ -140,7 +140,7 @@ void loginConta(const char *url) {
     if (logar == NULL) {
         printf("\nArquivo nao encontrado!!\n");
     } else {
-        // Lê cada linha do arquivo
+        // Lï¿½ cada linha do arquivo
         while (fgets(linha, sizeof(linha), logar) != NULL) {
             // Converte a linha para uma estrutura de conta
             sscanf(linha, "%49s %49s %24s %ld %ld %ld %ld %ld %ld %f",
@@ -149,83 +149,77 @@ void loginConta(const char *url) {
 
             // Verifica se o email e senha correspondem
             if (strcmp(Conta.email, email) == 0 && Conta.senha == senha) {
-                autenticado = 1;  // Marca que a autenticação foi bem-sucedida
+                autenticado = 1;  // Marca que a autenticaï¿½ï¿½o foi bem-sucedida
                 break;
             }
         }
         fclose(logar);  // Fecha o arquivo
 
-        // Se autenticado, chama a função IndexConta, senão exibe mensagem de erro
+        // Se autenticado, chama a funï¿½ï¿½o IndexConta, senï¿½o exibe mensagem de erro
         if (autenticado) {
             IndexConta(url, Conta.Onome, Conta.saldo, Conta.codigo_conta);
         } else {
             printf("E-mail ou senha incorretos.\n");
         }
     }
-
-    printf("\nPressione Enter para voltar ao menu principal...\n");
-    getchar();  // Espera o usuário pressionar Enter
-    interface();  // Retorna à interface principal
 }
 
-// Função que exibe o menu principal da conta após login
+// Funï¿½ï¿½o que exibe o menu principal da conta apï¿½s login
 void IndexConta(const char *url, const char *nome, float saldo, long int codigo_conta) {
     system("cls || clear");  // Limpa a tela do console
-    printf("\nSeja bem-vindo %s\n", nome);  // Saudação ao usuário
     int opr;
     do {
+        system("cls || clear");  // Limpa a tela do console
+        printf("\nSeja Bem-vinda %s!!!\n", nome);  // Saudaï¿½ï¿½o ao usuï¿½rio
         printf("+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+\n");
         printf("+|\t\tBANCO JMS\n");
         printf("+|\n");
         printf("+|\t1. Saldo\n");
         printf("+|\t2. Sacar\n");
         printf("+|\t3. Depositar\n");
-        printf("+|\t4. Extrato\n");
-        printf("+|\t5. Transferir\n");
+        printf("+|\t4. Transferir\n");
         printf("+|\t0. Sair\n");
         printf("+|\n");
         printf("+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+\nOperacao = ");
         scanf("%d", &opr);
         getchar(); // Para consumir o '\n' deixado pelo scanf
 
-        // Chama a função apropriada com base na entrada do usuário
+        // Chama a funï¿½ï¿½o apropriada com base na entrada do usuï¿½rio
         switch (opr) {
             case 1:
                 printf("Saldo atual: R$ %.2f\n", saldo);
+                Sleep(2000);
                 break;
             case 2:
-                printf("Saque\n");
                 sacar(url, codigo_conta);
+                Sleep(800);
+                Pagina_interface();
                 break;
             case 3:
-                printf("Deposito\n");
                 depositarConta(url,codigo_conta);
-                // Chame aqui a função de depósito
+                Sleep(800);
                 break;
             case 4:
-                printf("Extrato\n");
-                // Chame aqui a função de extrato
-                break;
-            case 5:
-                printf("Transferir\n");
-                // Chame aqui a função de transferência
+                transferir(url,codigo_conta);
+                Sleep(800);
                 break;
             case 0:
+                Pagina_interface();
                 break;
             default:
                 printf("\nOperacao invalida!!!\n");
                 break;
         }
-    } while (opr != 0);  // Continua exibindo o menu até que o usuário escolha sair
+    } while (opr != 0);  // Continua exibindo o menu atï¿½ que o usuï¿½rio escolha sair
 }
 
-// Função para realizar um saque
+// Funï¿½ï¿½o para realizar um saque
 void sacar(const char *url, long int codigo) {
     system("cls || clear");  // Limpa a tela do console
     float valor;
     char linha[300];
     FILE *arquivo = fopen(url, "r");  // Abre o arquivo original para leitura
-    FILE *temp = fopen("temp.txt", "w");  // Cria um arquivo temporário para escrita
+    FILE *temp = fopen("temp.txt", "w");  // Cria um arquivo temporï¿½rio para escrita
     if (arquivo == NULL || temp == NULL) {
         printf("\nArquivo nao encontrado\n");
         return;
@@ -238,13 +232,13 @@ void sacar(const char *url, long int codigo) {
 
     int contaEncontrada = 0;  // Flag para verificar se a conta foi encontrada
 
-    // Lê cada linha do arquivo original
+    // Lï¿½ cada linha do arquivo original
     while (fgets(linha, sizeof(linha), arquivo) != NULL) {
         // Converte a linha para uma estrutura de conta
         sscanf(linha, "%49s %49s %24s %ld %ld %ld %ld %ld %ld %f", Conta.Onome, Conta.email, Conta.emprego, &Conta.rg, &Conta.cpf,
                &Conta.telefone, &Conta.senha, &Conta.salario, &Conta.codigo_conta, &Conta.saldo);
 
-        // Verifica se o código da conta corresponde ao código passado
+        // Verifica se o cï¿½digo da conta corresponde ao cï¿½digo passado
         if (codigo == Conta.codigo_conta) {
             contaEncontrada = 1;  // Marca que a conta foi encontrada
             if (valor > Conta.saldo) {
@@ -255,7 +249,7 @@ void sacar(const char *url, long int codigo) {
             }
         }
 
-        // Escreve os dados (atualizados ou não) no arquivo temporário
+        // Escreve os dados (atualizados ou nï¿½o) no arquivo temporï¿½rio
         fprintf(temp, "%s %s %s %ld %ld %ld %ld %ld %ld %.2f\n",
                 Conta.Onome, Conta.email, Conta.emprego, Conta.rg, Conta.cpf,
                 Conta.telefone, Conta.senha, Conta.salario, Conta.codigo_conta, Conta.saldo);
@@ -265,13 +259,13 @@ void sacar(const char *url, long int codigo) {
     fclose(arquivo);
     fclose(temp);
 
-    // Se a conta foi encontrada e atualizada, substitui o arquivo original pelo temporário
+    // Se a conta foi encontrada e atualizada, substitui o arquivo original pelo temporï¿½rio
     if (contaEncontrada) {
-        remove(url); // ele apaga o arquivo "Dados.txt" que é o antigo;
-        rename("temp.txt", url); //Ele renomeia o arquivo "temp.txt" pelo nome armazenado na variavel url = "Dados.txt", fazendo assim uma atualização dos dados
+        remove(url); // ele apaga o arquivo "Dados.txt" que ï¿½ o antigo;
+        rename("temp.txt", url); //Ele renomeia o arquivo "temp.txt" pelo nome armazenado na variavel url = "Dados.txt", fazendo assim uma atualizaï¿½ï¿½o dos dados
     } else {
-        remove("temp.txt");  // Se a conta não foi encontrada, remove o temporário
-        printf("Conta não encontrada.\n");
+        remove("temp.txt");  // Se a conta nï¿½o foi encontrada, remove o temporï¿½rio
+        printf("Conta nï¿½o encontrada.\n");
     }
 }
 void depositarConta(const char *url,long int codigo){
@@ -285,7 +279,7 @@ void depositarConta(const char *url,long int codigo){
         return;
     }
     conta Conta;
-    printf("Valor do saque: ");
+    printf("Valor do Deposito: ");
     scanf("%f", &valor);
     getchar();  // Para consumir o '\n' deixado pelo scanf
     int encontrada = 0;
@@ -304,10 +298,64 @@ void depositarConta(const char *url,long int codigo){
     fclose(arquivo);
     fclose(temp);
     if(encontrada){
-        remove(url); // ele apaga o arquivo "Dados.txt" que é o antigo;
-        rename("temp.txt", url); //Ele renomeia o arquivo "temp.txt" pelo nome armazenado na variavel url = "Dados.txt", fazendo assim uma atualização dos dados
+        remove(url); // ele apaga o arquivo "Dados.txt" que ï¿½ o antigo;
+        rename("temp.txt", url); //Ele renomeia o arquivo "temp.txt" pelo nome armazenado na variavel url = "Dados.txt", fazendo assim uma atualizaï¿½ï¿½o dos dados
     } else {
-        remove("temp.txt");  // Se a conta não foi encontrada, remove o temporário
-        printf("Conta não encontrada.\n");
+        remove("temp.txt");  // Se a conta nï¿½o foi encontrada, remove o temporï¿½rio
+        printf("Conta nï¿½o encontrada.\n");
     }
+}
+void transferir(const char *url,long int codigo){
+    system("cls || clear");
+    float valor;
+    FILE *arquivo = fopen(url,"r");
+    FILE *temp = fopen("temp.txt","w");
+    char linha[300],nome[50];
+    long int codigo_transferir,cpf;
+
+    if (arquivo == NULL || temp == NULL) {
+        printf("\nArquivo nao encontrado\n");
+        return;
+    }
+
+    conta Conta;
+
+    printf("Digite o nome da pessoa que deseja transferir o dinheiro: ");
+    scanf("%s",&nome);
+    printf("CPF: ");
+    scanf("%ld",&cpf);
+    printf("Codigo da conta da pessoa que deseja transferir: ");
+    scanf("%ld",&codigo_transferir);
+    printf("Valor da transferencia: ");
+    scanf("%f", &valor);
+    getchar();  // Para consumir o '\n' deixado pelo scanf
+    int encontrada = 0;
+    while((fgets(linha,sizeof(linha),arquivo))!=NULL){
+        sscanf(linha, "%49s %49s %24s %ld %ld %ld %ld %ld %ld %f", Conta.Onome, Conta.email, Conta.emprego, &Conta.rg, &Conta.cpf,
+               &Conta.telefone, &Conta.senha, &Conta.salario, &Conta.codigo_conta, &Conta.saldo);
+        if(codigo == Conta.codigo_conta){ //primeiro vai ser descontado o valor da conta em que a transferencia esta sendo feita
+            encontrada = 1;  // Marca que a conta foi encontrada
+            Conta.saldo-=valor;
+        }
+        if(codigo_transferir == Conta.codigo_conta){//Depois eh adicionado o valor transferido na conta de destino
+                encontrada = 1;
+                Conta.saldo+=valor;
+        }
+        fprintf(temp, "%s %s %s %ld %ld %ld %ld %ld %ld %.2f\n",
+                Conta.Onome, Conta.email, Conta.emprego, Conta.rg, Conta.cpf,
+                Conta.telefone, Conta.senha, Conta.salario, Conta.codigo_conta, Conta.saldo);
+    }
+    fclose(arquivo);
+    fclose(temp);
+    if(encontrada){
+        remove(url); // ele apaga o arquivo "Dados.txt" que ï¿½ o antigo;
+        rename("temp.txt", url); //Ele renomeia o arquivo "temp.txt" pelo nome armazenado na variavel url = "Dados.txt", fazendo assim uma atualizaï¿½ï¿½o dos dados
+    } else {
+        remove("temp.txt");  // Se a conta nï¿½o foi encontrada, remove o temporï¿½rio
+        printf("Conta nï¿½o encontrada.\n");
+    }
+
+
+
+
 }
